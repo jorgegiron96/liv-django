4f79fbd63a1afc26b1b49c507e786b247216602c
Jorge Giron <jorgegiron96@gmail.com>
2022-12-24 15:11:31 -0300

    database

--- liv/settings.py
+++ liv/settings.py
@@ -66,14 +66,14 @@ WSGI_APPLICATION = 'liv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {}
+# DATABASES = {}
 
 
 # Password validation
