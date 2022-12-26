2832f672b06e5ff197412742b2e2aaaf7824fa66
Jorge Giron <jorgegiron96@gmail.com>
2022-12-25 22:02:51 -0300

    q

--- liv/settings/__init__.py
+++ liv/settings/__init__.py
@@ -5,11 +5,13 @@ import os
 try:
     if os.environ['ENV'] == 'dev':
         from .local import *
+        from .local import DEBUG
         print('está no LOCAL')
         print('DEBUG:', DEBUG)
 except:
     
     from .production import *
+    from .production import DEBUG
     print('está no PROD')
     print('DEBUG: ', DEBUG)
     
